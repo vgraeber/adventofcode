b103ae424b47a0dedbd68c9124eee5b515a44d65
Vivian Graeber <vgraeber@gmail.com>
2024-02-12 15:26:56 -0500

    work

--- 2023/day16/part1.py
+++ 2023/day16/part1.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 
 def getinput():
-  path = path = Path(__file__).parent / "sample.txt"
+  path = path = Path(__file__).parent / "input.txt"
   inputfile = open(path, 'r')
   rawinput = inputfile.read()
   inputfile.close()
@@ -15,22 +15,34 @@ def movebeam(origarr, beamarr, dir, pos):
   beamreact = {'.': {'N': 'N', 'S': 'S', 'E': 'E', 'W': 'W'}, '/': {'N': 'E', 'S': 'W', 'E': 'N', 'W': 'S'}, '\\': {'N': 'W', 'S': 'E', 'E': 'S', 'W': 'N'}, '|': {'N': 'N', 'S': 'S', 'E': ['N', 'S'], 'W': ['N', 'S']}, '-': {'N': ['E', 'W'], 'S': ['E', 'W'], 'E': 'E', 'W': 'W'}}
   arrbounds = {"row": [0, len(origarr)], "col": [0, len(origarr[0])]}
   if (beamarr[pos[0]][pos[1]][0] != '#'):
-    beamarr[pos[0]][pos[1]] = ['#', [dir]]
+    beamarr[pos[0]][pos[1]][0] = '#'
+    beamarr[pos[0]][pos[1]][1].append(dir)
   elif (dir not in beamarr[pos[0]][pos[1]][1]):
     beamarr[pos[0]][pos[1]][1].append(dir)
   else:
     return
-  pos[0] += moveindir[dir][0]
-  pos[1] += moveindir[dir][1]
   currpos = origarr[pos[0]][pos[1]]
   dir = beamreact[currpos][dir]
-  if ((pos[0] < arrbounds["row"][0]) or (arrbounds["row"][1] < pos[0]) or (pos[1] < arrbounds["col"][0]) or (arrbounds["col"][1] < pos[1])):
-    return
-  elif (isinstance(dir, list)):
-    movebeam(origarr, beamarr, dir[0], pos)
-    movebeam(origarr, beamarr, dir[1], pos)
+  if (isinstance(dir, list)):
+    pos1 = [(pos[0] + moveindir[dir[0]][0]), (pos[1] + moveindir[dir[0]][1])]
+    pos2 = [(pos[0] + moveindir[dir[1]][0]), (pos[1] + moveindir[dir[1]][1])]
+    if ((arrbounds["row"][0] <= pos1[0] < arrbounds["row"][1]) and (arrbounds["col"][0] <= pos1[1] < arrbounds["col"][1])):
+      movebeam(origarr, beamarr, dir[0], pos1.copy())
+    if ((arrbounds["row"][0] <= pos2[0] < arrbounds["row"][1]) and (arrbounds["col"][0] <= pos2[1] < arrbounds["col"][1])):
+      movebeam(origarr, beamarr, dir[1], pos2.copy())
   else:
-    movebeam(origarr, beamarr, dir, pos)
+    pos[0] += moveindir[dir][0]
+    pos[1] += moveindir[dir][1]
+    if ((arrbounds["row"][0] <= pos[0] < arrbounds["row"][1]) and (arrbounds["col"][0] <= pos[1] < arrbounds["col"][1])):
+      movebeam(origarr, beamarr, dir, pos.copy())
+
+def calcenergized(beamarr):
+  energized = 0
+  for row in beamarr:
+    for col in row:
+      if (col[0] == '#'):
+        energized += 1
+  return energized
 
 def printarr(arr, extra):
   toprint = ""
@@ -45,10 +57,9 @@ def printarr(arr, extra):
 
 def main():
   rawinputarray = getinput()
-  beamarray = [[['.']] * len(rawinputarray[0])] * len(rawinputarray)
-  printarr(rawinputarray, False)
-  printarr(beamarray, True)
+  beamarray = [[['.', []] for col in row] for row in rawinputarray]
   movebeam(rawinputarray, beamarray, 'E', [0, 0])
-  printarr(beamarray, True)
+  energized = calcenergized(beamarray)
+  print (energized)
 
 main()
