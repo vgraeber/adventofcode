98aace80e86fcd51e536189402b990adab8d88ec
Vivian Graeber <vgraeber@gmail.com>
2024-03-25 00:52:40 -0400

    work

---
+++ 2023/day25/part1.py
@@ -0,0 +1,47 @@
+from pathlib import Path
+
+def getinput():
+  path = path = Path(__file__).parent / "sample.txt"
+  inputfile = open(path, 'r')
+  rawinput = inputfile.read()
+  inputfile.close()
+  rawinputlist = rawinput.split("\n")
+  rawinputlist.pop()
+  lessrawinputlist = [line.split(": ") for line in rawinputlist]
+  filteredinputlist = [[line[0], line[1].split(" ")] for line in lessrawinputlist]
+  return filteredinputlist
+
+def formatinput(inputlist):
+  newinputlist = {}
+  for line in inputlist:
+    if (line[0] in newinputlist):
+      for cnctn in line[1]:
+        if (cnctn not in newinputlist[line[0]]):
+          newinputlist[line[0]].append(cnctn)
+        if (cnctn not in newinputlist):
+          newinputlist[cnctn] = [line[0]]
+        else:
+          newinputlist[cnctn].append(line[0])
+    else:
+      newinputlist[line[0]] = line[1]
+      for cnctn in line[1]:
+        if (cnctn not in newinputlist):
+          newinputlist[cnctn] = [line[0]]
+        else:
+          newinputlist[cnctn].append(line[0])
+  return newinputlist
+
+def printarr(arr):
+  toprint = ""
+  for row in arr.keys():
+    toprint += row
+    toprint += str(arr[row])
+    toprint += "\n"
+  print (toprint)
+
+def main():
+  inputlist = getinput()
+  inputlist = formatinput(inputlist)
+  printarr(inputlist)
+
+main()
